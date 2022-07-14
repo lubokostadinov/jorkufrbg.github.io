e783f9b5c71a1e296ad156fb271d6d58fb3f807d
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-07-14 16:22:31 +0300

    Who I Am

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,8 @@ export const aboutData = {
   description1:
     "My name's Georgi. I'm a web developer based in Sofia, Bulgaria.",
   description2:
-    "For the last two years I have been working on freelance projects and utilize the time to improve my developer skills to the next level. Right now I'm focused on learning React.js and related technologies. I spent my leisure hours with extreme sports and video games. I love to learn and explore new arenas.",
+    "For the last two years I have been working on freelance projects and utilize the time to improve my developer skills to the next level. Right now I'm focused on learning React.js and related technologies.",
+  description3: "I spent my leisure hours with extreme sports and video games.",
+  description4: "I love to learn and explore new arenas.",
   image: 2,
 };
