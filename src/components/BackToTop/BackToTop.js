700d63431c778401766a32431b0d5d7550bc3f42
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-07-27 03:23:37 +0300

    Orange Back To Top

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -7,15 +7,31 @@ import "./BackToTop.css";
 
 function BackToTop() {
   const [visible, setVisible] = useState(false);
+  const [color, setColor] = useState(false);
 
   const { theme } = useContext(ThemeContext);
 
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
+
     if (scrolled > 300) {
       setVisible(true);
     } else if (scrolled <= 300) {
       setVisible(false);
+    }
+  };
+
+  const toggleColor = () => {
+    let documentHeight = document.body.scrollHeight;
+    let currentScroll = window.scrollY + window.innerHeight;
+
+    let modifier = 200;
+    if (currentScroll + modifier > documentHeight) {
+      console.log("Orange");
+      setColor(true);
+    } else {
+      console.log("Normal");
+      setColor(false);
     }
   };
 
@@ -27,6 +43,7 @@ function BackToTop() {
   };
 
   window.addEventListener("scroll", toggleVisible);
+  window.addEventListener("scroll", toggleColor);
 
   const useStyles = makeStyles(() => ({
     icon: {
@@ -41,7 +58,11 @@ function BackToTop() {
 
   return (
     <div style={{ display: visible ? "inline" : "none" }} className="backToTop">
-      <button onClick={scrollToTop} aria-label="Back to top">
+      <button
+        className={color ? "orange" : ""}
+        onClick={scrollToTop}
+        aria-label="Back to top"
+      >
         <IoIosArrowDropupCircle className={classes.icon} />
       </button>
     </div>
