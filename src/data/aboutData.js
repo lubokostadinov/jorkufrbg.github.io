a0dcc6ad997efee50def43c5f6ea2e7b7d6dc58a
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-05 06:22:27 +0300

    Quotes Update

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,7 +3,7 @@ export const aboutData = {
   description1:
     "Proficient individual with over 6 years of experience in website development and interconnected technologies.",
   description2:
-    "I am deeply interested in real-time web and mobile applications, software design patterns & blockchain technologies. Currently my focus is on React.js and other related/complementary tools, such as Redux, Next.js, etc.",
+    "I am deeply interested in real-time web and mobile applications, software design patterns & blockchain technologies. Currently my focus is on React.js and other related/complementary tools, such as Redux, Router, Next.js, etc.",
   description3:
     "Everlearning with a strong desire to contribute to team and community growth.",
   image: 1,
