79ce75158a1884081c6d6254a9ab4f69c4981efe
Georgi Karadzhov <jorkufrbg@gmail.com>
2022-09-28 21:44:55 +0300

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -13,145 +13,145 @@ import hearteum from "../assets/svg/projects/hearteum.svg";
 import plygear from "../assets/svg/projects/plygear.svg";
 import michael from "../assets/svg/projects/michael.svg";
 
-export const projectsData = [
-  {
-    id: 1,
-    projectName: "React Meetups",
-    projectDesc:
-      "Browse and expand a huge list of highly active React meetups!",
-    tags: ["React", "Next", "MongoDB"],
-    code: "https://github.com/jorkufrbg/next-meetups",
-    demo: "https://meetups-nextjs-l8uk1bzc7-jorkufrbg.vercel.app/",
-    image: meetups,
-  },
-  {
-    id: 2,
-    projectName: "React Quotes",
-    projectDesc:
-      "React application for storing memorable quotes I came across.",
-    tags: ["React", "Router", "Firebase"],
-    code: "https://github.com/jorkufrbg/React-Quotes-App",
-    demo: "https://quotes-app-a62b0.web.app/",
-    image: quotes,
-  },
-  {
-    id: 3,
-    projectName: "React Jamming",
-    projectDesc:
-      "Simple application for searching and adding music to playlists, which utilizes the powerful Spotify API",
-    tags: ["React", "JavaScript", "Spotify API"],
-    code: "https://github.com/jorkufrbg/Jamming-React",
-    demo: "http://jamming-cc.surge.sh/",
-    image: jamming,
-  },
-  {
-    id: 4,
-    projectName: "Studio Rubik",
-    projectDesc:
-      "Creative Agency for Video Production, Rebranding and UX/UI Design.",
-    tags: ["Wordpress", "Javascript", "SCSS", "Webpack"],
-    code: "",
-    demo: "https://www.studiorubik.com/",
-    image: rubik,
-  },
-  {
-    id: 5,
-    projectName: "Flights and Adventures",
-    projectDesc: "Website for paragliding and extreme sports.",
-    tags: ["Wordpress", "CSS", "jQuery"],
-    code: "",
-    demo: "http://flightsandadventures.bg/",
-    image: flights,
-  },
-  {
-    id: 6,
-    projectName: "Ido Design",
-    projectDesc:
-      "High-end wayfinding solutions, interior design branding and POSM for public interior and exterior commercial spaces.",
-    tags: ["Python", "CSS", "JavaScript"],
-    code: "",
-    demo: "https://ido.design/",
-    image: ido,
-  },
-  {
-    id: 7,
-    projectName: "Wordborn",
-    projectDesc:
-      "This project is focused on teaching technical writing, public speaking and creative writing.",
-    tags: ["Wordpress", "SCSS", "JavaScript", "jQuery"],
-    code: "",
-    demo: "https://wordborn.org/",
-    image: wordborn,
-  },
-  {
-    id: 8,
-    projectName: "Whisp",
-    projectDesc:
-      "Fast-track self-service tool for sorting out your healthcare and residency abroad or back home!",
-    tags: ["Wordpress", "SCSS", "JavaScript", "jQuery"],
-    code: "",
-    demo: "https://www.whisphealth.com/",
-    image: whisp,
-  },
-  {
-    id: 9,
-    projectName: "Motoretta",
-    projectDesc: "The first of its kind electric moped in Bulgaria.",
-    tags: ["PHP", "jQuery", "SCSS"],
-    code: "",
-    demo: "https://motorettagroup.com/",
-    image: motoretta,
-  },
-  {
-    id: 10,
-    projectName: "Aakasha",
-    projectDesc:
-      "A unique clothing brand combining natural fabrics and impeccable quality.",
-    tags: ["PHP", "jQuery", "Bootstrap"],
-    code: "",
-    demo: "https://www.aakasha.com/",
-    image: aakasha,
-  },
-  {
-    id: 11,
-    projectName: "Sofia Art Gallery",
-    projectDesc:
-      "This website houses not only examples of contemporary and national revival art, but also the country's largest collection of medieval paintings.",
-    tags: ["SCSS", "jQuery"],
-    code: "",
-    demo: "https://nationalgallery.bg/",
-    image: artGallery,
-  },
-  {
-    id: 12,
-    projectName: "Michael Cohen Personal Website",
-    projectDesc:
-      "Michael Harris Cohen’s stories have been published in numerous magazines and anthologies.",
-    tags: ["HTML", "SCSS", "jQuery"],
-    code: "https://github.com/jorkufrbg/Michael-Harris-Website",
-    demo: "https://www.michaelharriscohen.net/",
-    image: michael,
-  },
-  {
-    id: 13,
-    projectName: "PlyGear",
-    projectDesc:
-      "PlyGear is a design studio and workshop for tailor-made furniture units made exclusively from plywood.",
-    tags: ["HTML", "SCSS", "JavaScript"],
-    code: "",
-    demo: "https://plygear.com/",
-    image: plygear,
-  },
-  {
-    id: 14,
-    projectName: "Hearteum",
-    projectDesc:
-      "Hearteum is a utility-payment solution on the Blockchain that stimulates commerce to improve customer value.",
-    tags: ["HTML", "SCSS", "JavaScript", "Webpack"],
-    code: "",
-    demo: "https://hearteum.studiorubik.com/",
-    image: hearteum,
-  },
+export const projectsData = [{
+  id: 1,
+  projectName: "React Quotes",
+  projectDesc:
+    "React application for storing memorable quotes I came across.",
+  tags: ["React", "Router", "Firebase"],
+  code: "https://github.com/jorkufrbg/React-Quotes-App",
+  demo: "https://quotes-app-a62b0.web.app/",
+  image: quotes,
+},
+{
+  id: 2,
+  projectName: "React Meetups",
+  projectDesc:
+    "Browse and expand a huge list of highly active React meetups!",
+  tags: ["React", "Next", "MongoDB"],
+  code: "https://github.com/jorkufrbg/next-meetups",
+  demo: "https://meetups-nextjs-l8uk1bzc7-jorkufrbg.vercel.app/",
+  image: meetups,
+},
+
+{
+  id: 3,
+  projectName: "React Jamming",
+  projectDesc:
+    "Simple application for searching and adding music to playlists, which utilizes the powerful Spotify API",
+  tags: ["React", "JavaScript", "Spotify API"],
+  code: "https://github.com/jorkufrbg/Jamming-React",
+  demo: "http://jamming-cc.surge.sh/",
+  image: jamming,
+},
+{
+  id: 4,
+  projectName: "Studio Rubik",
+  projectDesc:
+    "Creative Agency for Video Production, Rebranding and UX/UI Design.",
+  tags: ["Wordpress", "Javascript", "SCSS", "Webpack"],
+  code: "",
+  demo: "https://www.studiorubik.com/",
+  image: rubik,
+},
+{
+  id: 5,
+  projectName: "Flights and Adventures",
+  projectDesc: "Website for paragliding and extreme sports.",
+  tags: ["Wordpress", "CSS", "jQuery"],
+  code: "",
+  demo: "http://flightsandadventures.bg/",
+  image: flights,
+},
+{
+  id: 6,
+  projectName: "Ido Design",
+  projectDesc:
+    "High-end wayfinding solutions, interior design branding and POSM for public interior and exterior commercial spaces.",
+  tags: ["Python", "CSS", "JavaScript"],
+  code: "",
+  demo: "https://ido.design/",
+  image: ido,
+},
+{
+  id: 7,
+  projectName: "Wordborn",
+  projectDesc:
+    "This project is focused on teaching technical writing, public speaking and creative writing.",
+  tags: ["Wordpress", "SCSS", "JavaScript", "jQuery"],
+  code: "",
+  demo: "https://wordborn.org/",
+  image: wordborn,
+},
+{
+  id: 8,
+  projectName: "Whisp",
+  projectDesc:
+    "Fast-track self-service tool for sorting out your healthcare and residency abroad or back home!",
+  tags: ["Wordpress", "SCSS", "JavaScript", "jQuery"],
+  code: "",
+  demo: "https://www.whisphealth.com/",
+  image: whisp,
+},
+{
+  id: 9,
+  projectName: "Motoretta",
+  projectDesc: "The first of its kind electric moped in Bulgaria.",
+  tags: ["PHP", "jQuery", "SCSS"],
+  code: "",
+  demo: "https://motorettagroup.com/",
+  image: motoretta,
+},
+{
+  id: 10,
+  projectName: "Aakasha",
+  projectDesc:
+    "A unique clothing brand combining natural fabrics and impeccable quality.",
+  tags: ["PHP", "jQuery", "Bootstrap"],
+  code: "",
+  demo: "https://www.aakasha.com/",
+  image: aakasha,
+},
+{
+  id: 11,
+  projectName: "Sofia Art Gallery",
+  projectDesc:
+    "This website houses not only examples of contemporary and national revival art, but also the country's largest collection of medieval paintings.",
+  tags: ["SCSS", "jQuery"],
+  code: "",
+  demo: "https://nationalgallery.bg/",
+  image: artGallery,
+},
+{
+  id: 12,
+  projectName: "Michael Cohen Personal Website",
+  projectDesc:
+    "Michael Harris Cohen’s stories have been published in numerous magazines and anthologies.",
+  tags: ["HTML", "SCSS", "jQuery"],
+  code: "https://github.com/jorkufrbg/Michael-Harris-Website",
+  demo: "https://www.michaelharriscohen.net/",
+  image: michael,
+},
+{
+  id: 13,
+  projectName: "PlyGear",
+  projectDesc:
+    "PlyGear is a design studio and workshop for tailor-made furniture units made exclusively from plywood.",
+  tags: ["HTML", "SCSS", "JavaScript"],
+  code: "",
+  demo: "https://plygear.com/",
+  image: plygear,
+},
+{
+  id: 14,
+  projectName: "Hearteum",
+  projectDesc:
+    "Hearteum is a utility-payment solution on the Blockchain that stimulates commerce to improve customer value.",
+  tags: ["HTML", "SCSS", "JavaScript", "Webpack"],
+  code: "",
+  demo: "https://hearteum.studiorubik.com/",
+  image: hearteum,
+},
 ];
 
 /*
